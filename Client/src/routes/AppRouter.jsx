e2ff97f399a28d656be871b0757382ba81134a36
Deo-Siatah <deosiatah0@gmail.com>
2026-09-07 23:30:26 +0300

    fixed a naming issue

--- Client/src/routes/AppRouter.jsx
+++ Client/src/routes/AppRouter.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PublicLayout from "../layouts/PublicLayout";
 import AdminLayout from "../layouts/AdminLayout";
 import ProtectedRoute from "./ProtectedRoutes";
-import ProjectStoryPage from "../pages/ProjectStoryPage";
+import ProjectStoryPage from "../pages/ProjectStoryPage ";
 
 // Public Pages
 import Homepage from "../pages/Homepage";
